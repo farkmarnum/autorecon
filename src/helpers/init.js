af65d530659a153a197e38023947e29f9fcebad7
farkmarnum <mark.farnum@gmail.com>
2020-08-12 17:00:59 -0400

    Add error handling + retries for mongo connection

--- src/helpers/init.js
+++ src/helpers/init.js
@@ -4,11 +4,35 @@ import mongoose from 'mongoose'
 
 dotenv.config()
 
+const MAX_ATTEMPTS = 10
+
+const connect = async (attempt = 0) => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  } catch (err) {
+    console.error(err)
+
+    const newAttempt = attempt + 1
+    if (newAttempt > MAX_ATTEMPTS) {
+      console.error(`Connection failed after ${MAX_ATTEMPTS} attempts`)
+      throw new Error(err)
+    }
+
+    const waitTime = 5
+    console.error(`Waiting ${waitTime} seconds...`)
+    const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+    await wait(waitTime * 1000)
+
+    console.error('Retrying...')
+    await connect(newAttempt)
+  }
+}
+
 export const init = async () => {
   await requireDir(`${__dirname}/../models`)
 
-  await mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  await connect()
 }
